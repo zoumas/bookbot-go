03f71b820bc4cca49809fd9d4630e7c3e6f5764f
zoumas <zoumas@protonmail.com>
2026-04-17 16:14:44 +0300

    2.3: Read File

--- main.go
+++ main.go
@@ -1,7 +1,24 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"os"
+)
 
 func main() {
-	fmt.Println("Ready for bookbot")
+	contents, err := getBookText("books/frankenstein.txt")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println(contents)
+}
+
+func getBookText(filename string) (string, error) {
+	contents, err := os.ReadFile(filename)
+	if err != nil {
+		return "", fmt.Errorf("failed to read file: %w", err)
+	}
+	return string(contents), nil
 }
