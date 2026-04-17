376ca225e4e6cc0a7d48e2ea2fb8232107083eac
zoumas <zoumas@protonmail.com>
2026-04-17 16:49:22 +0300

    2.6: Count Characters

--- main.go
+++ main.go
@@ -13,6 +13,11 @@ func main() {
 	}
 
 	fmt.Println("Found", countWords(contents), "total words")
+
+	count := countCharacters(contents)
+	for k, v := range count {
+		fmt.Printf("%c: %d\n", k, v)
+	}
 }
 
 func getBookText(filename string) (string, error) {
--- stats.go
+++ stats.go
@@ -5,3 +5,11 @@ import "strings"
 func countWords(text string) int {
 	return len(strings.Fields(text))
 }
+
+func countCharacters(text string) map[rune]int {
+	m := make(map[rune]int)
+	for _, r := range strings.ToLower(text) {
+		m[r]++
+	}
+	return m
+}
